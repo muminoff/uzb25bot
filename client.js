1aca050a10cd33cccd34e0ed6d173d765effbc8e
Sardor Muminov <sardor@insilicogen.com>
2016-07-18 12:44:59 +0900

    No socket.io client

--- client.js
+++
@@ -1,8 +0,0 @@
-var socket = require('socket.io-client')('http://uzb25bot.herokuapp.com');
-socket.on('connect', function() {
-  console.log('connected');
-});
-
-socket.on('tweet', function(data) {
-  console.log(data);
-});
