abdc1e57944403fc14a21311839aca5ac11c58b7
Sardor Muminov <sardor@insilicogen.com>
2016-07-20 17:41:05 +0900

    ES6

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 // main modules
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var bodyParser = require('body-parser');
-var io = require('socket.io')(server);
-var pg = require('pg').native;
-
-// API
-var getLastTweets = require('./lib/tweets/getlasttweets');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const bodyParser = require('body-parser');
+const io = require('socket.io')(server);
+const pg = require('pg').native;
 
 // Environment variables
-var postgres_url = process.env.DATABASE_URL;
+const postgres_url = process.env.DATABASE_URL;
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', socket => {
   remote_addr = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address;
   console.info('Socket connected', socket.id);
   console.info('Socket address', remote_addr);
 
-  pg.connect(postgres_url, function(err, client, done) {
+  pg.connect(postgres_url, (err, client, done) => {
 
     if (err) {
       console.error('Cannot connect to Postgres (subscribe)');
@@ -26,24 +23,24 @@ io.sockets.on('connection', function(socket) {
       process.exit(-1);
     }
 
-    getLastTweets(client, socket.id, function(lastTweets) {
+    getLastTweets(client, socket.id, lastTweets => {
       console.info('Sending last 10 tweets to user', socket.id);
-      lastTweets.forEach(function(tweet) {
+      lastTweets.forEach(tweet => {
         io.sockets.emit('tweet', tweet);
       });
     });
 
   });
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', () => {
     console.info('Socket disconnected', socket.id);
   });
 });
 
 
 // Postgres channel listener
-var pgClient = new pg.Client(postgres_url);
-pgClient.connect(function(err) {
+const pgClient = new pg.Client(postgres_url);
+pgClient.connect(err => {
 
   if (err) {
     console.error('Cannot connect to Postgres (pubsub)');
@@ -51,7 +48,7 @@ pgClient.connect(function(err) {
     process.exit(-1);
   }
 
-  pgClient.query('LISTEN channel', function(err, result) {
+  pgClient.query('LISTEN channel', (err, result) => {
 
     if (err) {
       console.error('Cannot listen to channel');
@@ -62,15 +59,15 @@ pgClient.connect(function(err) {
 
   });
 
-  pgClient.on('notification', function(data) {
+  pgClient.on('notification', data => {
 
-    var tweetData = JSON.parse(data.payload);
+    const tweetData = JSON.parse(data.payload);
     console.log('Got tweet', tweetData.id, 'now broadcasting');
     io.sockets.emit('tweet', tweetData);
 
   });
 });
 
-server.listen(process.env.PORT, function() {
+server.listen(process.env.PORT, () => {
   console.log('Server listening at %d ...', process.env.PORT);
 });
